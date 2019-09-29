7f4980bab84aed3922e8febdb8f0a299b7131d36
Steven <spysschaert.steven0@gmail.com>
2019-09-29 23:09:02 +0200

    create an event Listener in the input for change the value to reach in the game thanks the function writeScoreToReach

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,12 +4,16 @@ let scoreJ1 = document.querySelector(".scoreJ1");
 let scoreJ2 = document.querySelector(".scoreJ2");
 let defaultScoreJ1 = 0;
 let defaultScoreJ2 = 0;
+let winScore = document.querySelector(".scoreVisé");
+let goal = document.querySelector(".objectif");
 const winner = document.querySelector(".winner");
 const J1Button = document.querySelector(".J1");
 const J2Button = document.querySelector(".J2");
 const resetButton = document.querySelector(".reset");
 
-function writeScoreToReach() {}
+function writeScoreToReach() {
+  winScore.textContent = goal.value;
+}
 
 function whoAreTheWinner() {}
 
@@ -18,3 +22,5 @@ function getScoreP1() {}
 function getScoreP2() {}
 
 function newGame() {}
+
+goal.addEventListener("change", writeScoreToReach);
