de1631af08831db1c8d35ec1c9524e5ca159851b
Steven <spysschaert.steven0@gmail.com>
2019-09-29 23:57:10 +0200

    change != in if conditon to <

--- assets/script/script.js
+++ assets/script/script.js
@@ -16,45 +16,44 @@ function writeScoreToReach() {
 }
 
 function whoAreTheWinner() {
-    if (scoreJ1 == goal.value){
-        winner.textContent = "Joueur 1";
-        scoreJ1.style.color= "Green";
-        scoreJ2.style.color= "Red";
-
-    }
-    else (scoreJ2 == goal.value){
-        winner.textContent = "Joueur 2";
-        scoreJ1.style.color= "Red";
-        scoreJ2.style.color= "Green";
-    }
+  if (scoreJ1 == goal.value) {
+    winner.textContent = "Joueur 1";
+    scoreJ1.style.color = "Green";
+    scoreJ2.style.color = "Red";
+  }
+  if (scoreJ2 == goal.value) {
+    winner.textContent = "Joueur 2";
+    scoreJ1.style.color = "Red";
+    scoreJ2.style.color = "Green";
+  }
 }
 
 function getScoreP1() {
-    if (scoreJ1 != goal.value && scoreJ2 != goal.value){
-        scoreJ1++;
-    }
+  if (scoreJ1 < goal.value && scoreJ2 < goal.value) {
+    scoreJ1++;
+  }
 
-    scoreJ1.textContent = defaultScoreJ1;
-    whoAreTheWinner();
+  scoreJ1.textContent = defaultScoreJ1;
+  whoAreTheWinner();
 }
 
 function getScoreP2() {
-    if (scoreJ2 != goal.value && scoreJ1 != goal.value){
-        scoreJ2++;
-    }
-    scoreJ2.textContent = defaultScoreJ2;
-    whoAreTheWinner();
-
+  if (scoreJ2 < goal.value && scoreJ1 < goal.value) {
+    scoreJ2++;
+  }
+  scoreJ2.textContent = defaultScoreJ2;
+  whoAreTheWinner();
+}
 function newGame() {
-    scoreJ1.textContent=0;
-    scoreJ2.textContent=0;
-    defaultScoreJ1=0;
-    defaultScoreJ2=0;
-    winScore = 10;
-    winner.textContent="xxxx";
-    goal.value=10;
-    scoreJ1.style.color=null;
-    scoreJ2.style.color=null;
+  scoreJ1.textContent = 0;
+  scoreJ2.textContent = 0;
+  defaultScoreJ1 = 0;
+  defaultScoreJ2 = 0;
+  winScore = 10;
+  winner.textContent = "xxxx";
+  goal.value = 10;
+  scoreJ1.style.color = null;
+  scoreJ2.style.color = null;
 }
 
 J1Button.addEventListener("click", getScoreP1);
