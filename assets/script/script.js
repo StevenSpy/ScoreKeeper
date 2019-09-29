f8b8eae1022936e2878cf4c3001706cb6566b537
Steven <spysschaert.steven0@gmail.com>
2019-09-29 22:56:46 +0200

    Variables init and allotment

---
+++ assets/script/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+let scoreJ1 = document.querySelector(".scoreJ1");
+let scoreJ2 = document.querySelector(".scoreJ2");
+let defaultScoreJ1 = 0;
+let defaultScoreJ2 = 0;
+const winner = document.querySelector(".winner");
+const J1Button = document.querySelector(".J1");
+const J2Button = document.querySelector(".J2");
+const resetButton = document.querySelector(".reset");
